131a9be8b1604a1b252ce7ec312464bd9c8efc8a
Garrett <lamgarr@outlook.com>
2022-10-13 19:45:42 -0700

    Update router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import Nav from './Nav';
+import Home from './Home';
+import About from './About';
+import Shop from './Shop';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
