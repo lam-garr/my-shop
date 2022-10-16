f4e4721520d7a57d4e0d30fd3cda001d2fe73ba6
Garrett <lamgarr@outlook.com>
2022-10-15 22:27:07 -0700

    Add nav dropdown

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import '../styles/Nav.css';
 
 function Nav(){
+
+    const [open, setOpen] = useState(false);
+
     return(
         <nav className = "nav-bar">
             <div className='nav-container'>  
@@ -10,6 +14,13 @@ function Nav(){
                     <Link to='/'>
                         <h1>Shop Name</h1>
                     </Link>
+                </div>
+                <div className='dropdown'>
+                    <button className='dropbtn' onClick={()=>{setOpen(state => !state)}}>Menu</button>
+                    <div className={`drop-content ${open? 'active' : 'inactive'}`}>
+                        <button><Link to='/about'>About</Link></button>
+                        <button><Link to='/shop'>Shop</Link></button>
+                    </div>
                 </div>
             </div>
         </nav>
