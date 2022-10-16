b31f16e53bd50937f8ea91a0154c018539066be8
Garrett <lamgarr@outlook.com>
2022-10-15 19:32:53 -0700

    Update nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../styles/Nav.css';
 
 function Nav(){
     return(
         <nav className = "nav-bar">
             <div className='nav-container'>  
+                <div className='nav-logo'>
+                    <Link to='/'>
+                        <h1>Shop Name</h1>
+                    </Link>
+                </div>
             </div>
         </nav>
     )
